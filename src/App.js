6f84fdb9fd16c43288fbc8f795f6c68ac5d1f633
Ashutosh Thakur <ashutoshthakur_14@yahoo.com>
2020-08-12 17:56:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import "./styles.css";
+import NavBar from "./NavBar";
+
+let id = 0;
+
+const Todo = (props) => (
+  <li>
+    <input
+      type="checkbox"
+      checked={props.todo.checked}
+      onClick={props.onToggle}
+    />
+    <span>{props.todo.text}</span>
+    <button onClick={props.onDelete}>delete</button>
+  </li>
+);
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [] || JSON.parse(localStorage.getItem("todos"))
+    };
+    this.addTodo = this.addTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
+  }
+  addTodo() {
+    const text = prompt("Add todo ");
+    this.setState({
+      todos: [...this.state.todos, { id: id++, text: text, checked: false }]
+    });
+  }
+  removeTodo(id) {
+    this.setState({
+      todos: this.state.todos.filter((todo) => todo.id !== id)
+    });
+  }
+  toggleTodo(id) {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id !== id) return todo;
+        return {
+          id: todo.id,
+          text: todo.text,
+          checked: !todo.checked
+        };
+      })
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <h1>Hello User</h1>
+        <p>This is To-Do Application made using React JS</p>
+        <br />
+        <div>Total Todos are : {this.state.todos.length}</div>
+        <div>
+          Total Unchecked Todos :{" "}
+          {this.state.todos.filter((todo) => !todo.checked).length}
+        </div>
+        <div className="bod">
+          <button onClick={this.addTodo}>ADD Todo</button>
+          <ul>
+            {this.state.todos.map((todo) => (
+              <Todo
+                todo={todo}
+                onDelete={() => this.removeTodo(todo.id)}
+                onToggle={() => this.toggleTodo(todo.id)}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/NavBar.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">TODO APP</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
